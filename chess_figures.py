9f3dacb6a73923d246f94d7ace90463231b9ec92
Prrromanssss <97535689+Prrromanssss@users.noreply.github.com>
2022-05-13 22:23:32 +0300

    Add files via upload

---
+++ chess_figures.py
@@ -0,0 +1,343 @@
+WHITE = 1
+BLACK = 2
+
+
+def correct_coords(row, col):
+    return 0 <= row < 8 and 0 <= col < 8
+
+
+def opponent(color):
+    if color == WHITE:
+        return BLACK
+    return WHITE
+
+
+class Board:
+    def __init__(self):
+        self.color = WHITE
+        self.previous_move_for_pass, self.pass_figure = False, (None, None)
+        self.field = [[None] * 8 for i in range(8)]
+        self.field[0] = [
+            Rook(BLACK), Knight(BLACK), Bishop(BLACK), Queen(BLACK),
+            King(BLACK), Bishop(BLACK), Knight(BLACK), Rook(BLACK)
+        ]
+        self.field[1] = [
+            Pawn(BLACK), Pawn(BLACK), Pawn(BLACK), Pawn(BLACK),
+            Pawn(BLACK), Pawn(BLACK), Pawn(BLACK), Pawn(BLACK)
+        ]
+        self.field[6] = [
+            Pawn(WHITE), Pawn(WHITE), Pawn(WHITE), Pawn(WHITE),
+            Pawn(WHITE), Pawn(WHITE), Pawn(WHITE), Pawn(WHITE)
+        ]
+        self.field[7] = [
+            Rook(WHITE), Knight(WHITE), Bishop(WHITE), Queen(WHITE),
+            King(WHITE), Bishop(WHITE), Knight(WHITE), Rook(WHITE)
+        ]
+
+    def get_piece(self, row, col):
+        return self.field[row][col]
+
+    def can_castling0(self, row, col):
+        start_ind = 0 if self.color == BLACK else 7
+        if start_ind != row or col != 2:
+            return False
+        if type(self.field[start_ind][0]) is Rook and type(self.field[start_ind][4]) is King \
+                and all(self.field[start_ind][i] is None for i in range(1, 4)) \
+                and self.field[start_ind][0].flag_for_cast \
+                and self.field[start_ind][4].flag_for_cast:
+            return True
+        return False
+
+    def castling0(self):
+        start_ind = 0 if self.color == BLACK else 7
+        if self.can_castling0(start_ind, 2):
+            self.color = opponent(self.color)
+            self.field[start_ind][0], self.field[start_ind][3] = None, self.field[start_ind][0]
+            self.field[start_ind][4], self.field[start_ind][2] = None, self.field[start_ind][4]
+            return True
+        return False
+
+    def can_castling7(self, row, col):
+        start_ind = 0 if self.color == BLACK else 7
+        if start_ind != row or col != 6:
+            return False
+        if type(self.field[start_ind][-1]) is Rook and type(self.field[start_ind][4]) is King \
+                and all(self.field[start_ind][i] is None for i in range(5, 7)) \
+                and self.field[start_ind][-1].flag_for_cast \
+                and self.field[start_ind][4].flag_for_cast:
+            return True
+        return False
+
+    def castling7(self):
+        start_ind = 0 if self.color == BLACK else 7
+        if self.can_castling7(start_ind, 6):
+            self.color = opponent(self.color)
+            self.field[start_ind][-1], self.field[start_ind][-3] = None, self.field[start_ind][-1]
+            self.field[start_ind][4], self.field[start_ind][-2] = None, self.field[start_ind][4]
+            return True
+        return False
+
+    def taking_on_the_pass(self, row, col):
+        if type(self.field[row][col]) is Pawn and self.field[row][col].flag_for_pass:
+            self.previous_move_for_pass, self.pass_figure = True, (row, col)
+            return True
+        return False
+
+    def cell(self, row, col):
+        piece = self.field[row][col]
+        if piece is not None:
+            return piece.char()
+
+    def move_piece(self, row, col, row1, col1):
+        if not correct_coords(row, col) or not correct_coords(row1, col1):
+            return False
+        if row == row1 and col == col1:
+            return False
+        piece = self.field[row][col]
+        if piece is None:
+            return False
+        if piece.get_color() != self.color:
+            return False
+        if type(self.field[row][col]) is Pawn and self.field[row][col].can_taking_pass(self, row, col, row1, col1):
+            self.field[self.pass_figure[0]][self.pass_figure[1]] = None
+            self.previous_move_for_pass = False
+        elif self.field[row1][col1] is None:
+            if not piece.can_move(self, row, col, row1, col1):
+                return False
+        elif self.field[row1][col1] is not None:
+            if not piece.can_attack(self, row, col, row1, col1):
+                return False
+        self.field[row][col] = None
+        self.field[row1][col1] = piece
+        if not self.taking_on_the_pass(row1, col1):
+            self.previous_move_for_pass = False
+        if type(piece) is King or type(piece) is Rook:
+            piece.flag_for_cast = False
+        self.color = opponent(self.color)
+        return True
+
+
+class Knight:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return '\u265E'
+
+    def can_move(self, board, row, col, row1, col1):
+        if not correct_coords(row1, col1):
+            return False
+        if board.field[row1][col1] is not None and board.field[row1][col1].get_color()\
+                == board.field[row][col].get_color():
+            return False
+        if (abs(col - col1) == 1 and abs(row - row1) == 2) or (abs(col - col1) == 2 and abs(row - row1) == 1):
+            return True
+        return False
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+    def __repr__(self):
+        return f"Knight({self.color})"
+
+
+class Bishop:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return '\u265D'
+
+    def can_move(self, board, row, col, row1, col1):
+        if not (correct_coords(row1, col1) and (row != row1 and col != col1)
+                and abs(col - col1) == abs(row - row1)):
+            return False
+        if board.field[row1][col1] is not None and board.field[row1][col1].get_color()\
+                == board.field[row][col].get_color():
+            return False
+        coords = []
+        if row1 < row and col1 > col:
+            coords = [(row - i, col + i) for i in range(1, 9) if row - i >= row1 and col + i <= col1]
+            coords.remove((row1, col1))
+        elif row1 > row and col1 > col:
+            coords = [(row + i, col + i) for i in range(1, 9) if row + i <= row1 and col + i <= col1]
+            coords.remove((row1, col1))
+        elif row1 > row and col1 < col:
+            coords = [(row + i, col - i) for i in range(1, 9) if row + i <= row1 and col - i >= col1]
+            coords.remove((row1, col1))
+        elif row1 < row and col1 < col:
+            coords = [(row - i, col - i) for i in range(1, 9) if row - i >= row1 and col - i >= col1]
+            coords.remove((row1, col1))
+        for i in range(len(board.field)):
+            for j in range(len(board.field[i])):
+                if board.field[i][j] is not None:
+                    if (i, j) in coords:
+                        return False
+        return True
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+    def __repr__(self):
+        return f"Bishop({self.color})"
+
+
+class Queen:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def char(self):
+        return '\u265B'
+
+    def can_move(self, board, row, col, row1, col1):
+        if row == row1 and col == col1:
+            return False
+        if Rook(self.color).can_move(board, row, col, row1, col1)\
+                or Bishop(self.color).can_move(board, row, col, row1, col1):
+            return True
+        return False
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+    def __repr__(self):
+        return f"Queen({self.color})"
+
+
+class Rook:
+    def __init__(self, color):
+        self.color = color
+        self.flag_for_cast = True
+
+    def char(self):
+        return '\u265C'
+
+    def get_color(self):
+        return self.color
+
+    def can_move(self, board, row, col, row1, col1):
+        if row != row1 and col != col1 and correct_coords(row1, col1):
+            return False
+        if board.field[row1][col1] is not None and board.field[row1][col1].get_color()\
+                == board.field[row][col].get_color():
+            return False
+        coords = []
+        if row1 < row and col1 == col:
+            coords = [(row - i, col) for i in range(1, 9) if row - i >= row1]
+            coords.remove((row1, col1))
+        elif row1 > row and col1 == col:
+            coords = [(row + i, col) for i in range(1, 9) if row + i <= row1]
+            coords.remove((row1, col1))
+        elif row1 == row and col1 < col:
+            coords = [(row, col - i) for i in range(1, 9) if col - i >= col1]
+            coords.remove((row1, col1))
+        elif row1 == row and col1 > col:
+            coords = [(row, col + i) for i in range(1, 9) if col + i <= col1]
+            coords.remove((row1, col1))
+        for i in range(len(board.field)):
+            for j in range(len(board.field[i])):
+                if board.field[i][j] is not None:
+                    if (i, j) in coords:
+                        return False
+        return True
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+    def __repr__(self):
+        return f"Rook({self.color})"
+
+
+class Pawn:
+    def __init__(self, color):
+        self.color = color
+        self.flag_for_pass = False
+
+    def char(self):
+        return '\u265F'
+
+    def get_color(self):
+        return self.color
+
+    def can_taking_pass(self, board, row, col, row1, col1):
+        direction = 1 if self.color == BLACK else -1
+        var = ['col + 1 if col + 1 < 8 else col', 'col - 1 if col + 1 >= 0 else col']
+        right_pawn = board.field[row][eval(var[0])]
+        left_pawn = board.field[row][eval(var[1])]
+        if not (row + direction == row1 and ((col + 1 == col1 and type(right_pawn) is Pawn)
+                                             or (col - 1 == col1 and type(left_pawn) is Pawn))):
+            return False
+        if not (type(right_pawn) is Pawn and right_pawn.get_color() != self.get_color() or
+                type(left_pawn) is Pawn and left_pawn.get_color() != self.get_color()):
+            return False
+        if abs(col - col1) == 1 and board.previous_move_for_pass and board.field[row1][col1] is None\
+                and board.field[board.pass_figure[0]][board.pass_figure[1]].get_color() != self.get_color():
+            return True
+        return False
+
+    def can_move(self, board, row, col, row1, col1):
+        if col != col1:
+            return False
+        if self.color == BLACK:
+            start_row, direction = 1, 1
+        else:
+            start_row, direction = 6, -1
+        if (row1 - row) * direction == 1 and board.field[row1][col1] is None:
+            self.flag_for_pass = False
+            return True
+        if start_row == row and board.field[row + direction][col1] is None and row + 2 * direction == row1:
+            self.flag_for_pass = True
+            return True
+        return False
+
+    def can_attack(self, board, row, col, row1, col1):
+        if board.field[row1][col1] is not None and board.field[row1][col1].get_color()\
+                == board.field[row][col].get_color():
+            return False
+        if board.field[row1][col1] is None:
+            return False
+        direction = 1 if self.color == BLACK else -1
+        if row + direction == row1 and (col + 1 == col1 or col - 1 == col1):
+            self.flag_for_pass = False
+            return True
+        return False
+
+    def __repr__(self):
+        return f"Pawn({self.color})"
+
+
+class King:
+    def __init__(self, color):
+        self.color = color
+        self.flag_for_cast = True
+
+    def char(self):
+        return '\u265A'
+
+    def get_color(self):
+        return self.color
+
+    def can_move(self, board, row, col, row1, col1):
+        if not (correct_coords(row1, col1)):
+            return False
+        if board.field[row1][col1] is not None and board.field[row1][col1].get_color()\
+            == board.field[row][col].get_color():
+            return False
+        coords = [(row + 1, col + 1), (row + 1, col - 1), (row - 1, col + 1), (row - 1, col - 1),
+                  (row, col + 1), (row, col - 1), (row + 1, col), (row - 1, col)]
+        return (row1, col1) in list(filter(lambda x: 0 <= x[0] < 8 and 0 <= x[1] < 8, coords))
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+    def __repr__(self):
+        return f"King({self.color})"
---
+++ chess_main.py
@@ -0,0 +1,188 @@
+from chess.chess_figures import *
+import tkinter as tk
+
+
+def print_board(act_fig=None):
+    condition = '''(act_fig == (row, col) or board.field[act_fig[0]][act_fig[1]].can_move(board, act_fig[0],
+                  act_fig[1], row, col) or board.field[act_fig[0]][act_fig[1]].can_attack(board, act_fig[0],
+                  act_fig[1], row, col)) or (type(board.field[act_fig[0]][act_fig[1]]) is Pawn
+                  and board.field[act_fig[0]][act_fig[1]].can_taking_pass(board, act_fig[0],
+                  act_fig[1], row, col)) or (type(board.field[act_fig[0]][act_fig[1]]) is King
+                  and (board.can_castling7(row, col) or board.can_castling0(row, col)))'''
+    canvas.delete('all')
+    for row in range(8):
+        for col in range(8):
+            canvas.create_rectangle(col * 95, row * 95, 95 + col * 95, 95 + row * 95,
+                                    fill=('#856247' if (not col % 2 and not row % 2) or (col % 2 and row % 2)
+                                          else '#b5a78c') if act_fig and (eval(condition) if act_fig != 'all' else 1)
+                                    else ('#b78763' if (not col % 2 and not row % 2) or (col % 2 and row % 2)
+                                          else '#eedab5'),
+                                    outline=('#856247' if (not col % 2 and not row % 2) or (col % 2 and row % 2)
+                                             else '#b5a78c') if act_fig and (eval(condition) if act_fig != 'all' else 1)
+                                    else ('#b78763' if (not col % 2 and not row % 2) or (col % 2 and row % 2)
+                                          else '#eedab5'))
+
+
+def print_figures(board):
+    for row in range(8):
+        for col in range(8):
+            canvas_field[row][col] = canvas.create_text(47 + col * 95, 47 + row * 95,
+                                                        fill=('white' if board.get_piece(row, col).get_color() == WHITE
+                                                              else 'black') if board.get_piece(row, col) else '',
+                                                        text=board.cell(row, col) if board.get_piece(row, col) else '',
+                                                        font='Helvetica 70 bold')
+            coordinates_to_move[row][col] = (47 + col * 95, 47 + row * 95)
+
+
+def can_promote():
+    if not (type(board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]]) is Pawn and arr_of_two_fig[1][0] in [7, 0]):
+        return False
+    redraw_canvas('all')
+    new_widgets_prom[0] = canvas.create_rectangle(0, 190, 761, 570, fill='white')
+    new_widgets_prom[1] = canvas.create_text(380, 270, fill='black', text='Choose the figure to continue.',
+                                             font='Helvetica 40')
+    for figure in range(4):
+        new_widgets_prom[figure + 2] = canvas.create_text(100 + figure * 190, 380,
+                                                          text=['\u265B', '\u265C', '\u265D', '\u265E'][figure],
+                                                          fill='black', font='Helvetica 150 bold')
+    return True
+
+
+def promote_pawn(event):
+    chars = {'\u265C': Rook, '\u265E': Knight, '\u265D': Bishop, '\u265B': Queen}
+    pos_chars = {'\u265B': (range(0, 191), range(190, 571)), '\u265C': (range(191, 381), range(190, 571)),
+                 '\u265D': (range(381, 571), range(190, 571)), '\u265E': (range(571, 761), range(190, 571))}
+    new_figure = None
+    for pos in pos_chars:
+        if event.x in pos_chars[pos][0] and event.y in pos_chars[pos][1]:
+            new_figure = chars[pos]
+    if not new_figure:
+        return False
+    board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]]\
+        = new_figure(board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]].get_color())
+    canvas.delete(canvas_field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]])
+    obj = canvas.create_text(*coordinates_to_move[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]], fill='white'
+                             if board.get_piece(arr_of_two_fig[1][0],
+                                                arr_of_two_fig[1][1]).get_color() == WHITE else 'black',
+                             text=board.cell(arr_of_two_fig[1][0], arr_of_two_fig[1][1]),
+                             font='Helvetica 70 bold')
+    canvas_field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]] = obj
+    for widget in new_widgets_prom:
+        canvas.delete(widget)
+    redraw_canvas()
+    arr_of_two_fig.clear()
+    return True
+
+
+def castling(row, col, row1, col1):
+    if (col1 not in [2, 6]) or (type(board.field[row][col]) is not King):
+        return
+    if col1 == 6:
+        func = 7
+    else:
+        func = 0
+    color = board.field[row][col].get_color()
+    if not eval(f'board.castling{func}()'):
+        return False
+    canvas.coords(canvas_field[row][col],
+                  *coordinates_to_move[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]])
+    canvas.coords(canvas_field[0 if color == BLACK else 7][func],
+                  *coordinates_to_move[0 if color == BLACK else 7][5 if func == 7 else 3])
+    canvas_field[row][col], canvas_field[row1][col1] = None, canvas_field[row][col]
+    canvas_field[0 if color == BLACK else 7][-1 if func == 7 else 0],\
+        canvas_field[0 if color == BLACK else 7][5 if func == 7 else 3] = None,\
+        canvas_field[0 if color == BLACK else 7][-1 if func == 7 else 0]
+    arr_of_two_fig.clear()
+    return True
+
+
+def redraw_canvas(figure=None):
+    print_board(act_fig=figure)
+    print_figures(board)
+    pass  # ToDo: frame
+
+
+def main(event):
+    global flag_for_action
+    flag_for_redraw = True
+    if len(list(filter(lambda x: sum(map(lambda z: 1, filter(lambda y: type(y) is King, x))), board.field))) <= 1:
+        return
+    if flag_for_action:
+        if promote_pawn(event):
+            flag_for_action = False
+        return
+    for row in range(len(coordinates_of_field)):
+        for col in range(len(coordinates_of_field[row])):
+            if event.x in coordinates_of_field[row][col][0] and event.y in coordinates_of_field[row][col][1]:
+                arr_of_two_fig.append((row, col))
+    if not arr_of_two_fig:
+        return
+    if board.field[arr_of_two_fig[0][0]][arr_of_two_fig[0][1]] is not None:
+        redraw_canvas(arr_of_two_fig[0])
+    if board.field[arr_of_two_fig[0][0]][arr_of_two_fig[0][1]] is None:
+        arr_of_two_fig.clear()
+        return
+    if len(arr_of_two_fig) == 2:
+        redraw_canvas()
+        if board.field[arr_of_two_fig[0][0]][arr_of_two_fig[0][1]] is not None\
+            and board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]] is not None\
+            and board.field[arr_of_two_fig[0][0]][arr_of_two_fig[0][1]].get_color()\
+                == board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]].get_color():
+            arr_of_two_fig[0] = arr_of_two_fig[1]
+            redraw_canvas(arr_of_two_fig[0])
+            del arr_of_two_fig[-1]
+            return
+        flag_for_end = False
+        if type(board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]]) is King:
+            flag_for_end, color = True, board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]].get_color()
+        if castling(*arr_of_two_fig[0], *arr_of_two_fig[1]):
+            return
+        if board.move_piece(*arr_of_two_fig[0], *arr_of_two_fig[1]):
+            if can_promote():
+                flag_for_action, flag_for_redraw = True, False
+            if canvas_field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]] is not None:
+                canvas.delete(canvas_field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]])
+                if flag_for_redraw:
+                    redraw_canvas()
+            if type(board.field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]]) is Pawn \
+                    and arr_of_two_fig[1][1] != arr_of_two_fig[0][1] \
+                    and canvas_field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]] is None:
+                if flag_for_redraw:
+                    redraw_canvas()
+                canvas.delete(canvas_field[board.pass_figure[0]][board.pass_figure[1]])
+                canvas_field[board.pass_figure[0]][board.pass_figure[1]] = None
+            canvas.coords(canvas_field[arr_of_two_fig[0][0]][arr_of_two_fig[0][1]],
+                          *coordinates_to_move[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]])
+            if flag_for_end:
+                redraw_canvas('all')
+                canvas.create_rectangle(0, 190, 760, 570, fill='white')
+                canvas.create_text(370, 380, text=f"{'Whites' if color == WHITE else 'Blacks'} won!",
+                                   fill='black', font='Helvetica 70 bold', justify=tk.CENTER)
+            canvas_field[arr_of_two_fig[0][0]][arr_of_two_fig[0][1]], \
+                canvas_field[arr_of_two_fig[1][0]][arr_of_two_fig[1][1]] = None, \
+                canvas_field[arr_of_two_fig[0][0]][arr_of_two_fig[0][1]]
+        if not flag_for_action:
+            arr_of_two_fig.clear()
+
+
+flag_for_action = False
+new_widgets_prom = [None] * 6
+coordinates_to_move, canvas_field = [[None] * 8 for _ in range(8)], [[None] * 8 for _ in range(8)]
+coordinates_of_field, arr_of_two_fig = [[] * 8 for i in range(8)], []
+for i in range(8):
+    for j in range(8):
+        coordinates_of_field[i].append((range(j * 95 + 1 if j else 0, 96 + j * 95), range(i * 95, 96 + i * 95)))
+
+
+if __name__ == '__main__':
+    win = tk.Tk()
+    win.title('Chess')
+    win.iconphoto(False, tk.PhotoImage(file='chess/icon.png'))
+    win.geometry('761x760+350+0')
+    canvas = tk.Canvas(win, width=761, height=760, bd=0, highlightthickness=0)
+    board = Board()
+    canvas.pack()
+    canvas.bind_all("<Button-1>", main)
+    print_board()
+    print_figures(board)
+    win.mainloop()
